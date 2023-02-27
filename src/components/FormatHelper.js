705a252afd11e982516b7ee7e00a0890ee1730de
chriszack3 <czack@chrispc>
2023-02-27 18:42:37 -0500

    visualized streamed json

--- src/components/FormatHelper.js
+++ src/components/FormatHelper.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios"
 
 const FormatHelper = () => {
+    const [jsonRes, setJsonRes] = useState([])
     const getJson = async () => {
         const response = await axios.get('/api/ReturnJson')
-        console.log(response)
+        setJsonRes(response.data)
     }
     return (
         <div>
+            {
+                jsonRes && jsonRes.map((threadObj, i) => {
+                    console.log(threadObj)
+                    return (
+                        <div key={i}>
+                            <h4>{threadObj.usernames}</h4>
+                            <span>Messages: </span>
+                            {
+                                threadObj?.threadMessages.map(messages => (
+                                    <p>{messages.text}</p>
+                                ))
+                            }
+                        </div>
+                    )
+
+                })
+            }
             <button onClick={() => getJson()}>Get Json</button>
         </div>
     )
