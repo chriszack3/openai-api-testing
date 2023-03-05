8ea033a5837d718749cd4fae8a083b78960f914b
chriszack3 <czack@chrispc>
2023-03-05 14:47:55 -0500

    added prompt/comp formatting

--- src/components/FormatHelper.js
+++ src/components/FormatHelper.js
@@ -11,15 +11,40 @@ const FormatHelper = () => {
         <div>
             {
                 jsonRes && jsonRes.map((threadObj, i) => {
-                    console.log(threadObj)
+                    let prompts = []
+                    let completions = []
+                    let pairs = []
+                    threadObj.threadMessages.forEach((messages, index) => {
+                        if (messages?.html?.includes("Appropriate_One_8365")) {
+                            completions.push(messages.text)
+                            if (!messages?.[index + 1]?.html?.includes("Appropriate_One_8365")) {
+                                pairs = [...pairs, { prompts, completions }]
+                                prompts = []
+                                completions = []
+                            }
+                        } else {
+                            prompts.push(messages.text)
+                        }
+                    })
+                    console.log(pairs)
                     return (
                         <div key={i}>
                             <h4>{threadObj.usernames}</h4>
                             <span>Messages: </span>
                             {
-                                threadObj?.threadMessages.map(messages => (
-                                    <p>{messages.text}</p>
-                                ))
+                                pairs?.map(messages => {
+                                    let promptsString = ""
+                                    let comptString = ""
+                                    messages?.prompts?.forEach((prompt, i) => {
+                                        i > 0 ? promptsString = promptsString.concat('**start new message**' + prompt) : promptsString = promptsString.concat(prompt)
+                                    })
+                                    messages?.completions?.forEach((comp, i) => {
+                                        i > 0 ? comptString = comptString.concat('**start new message**' + comp) : comptString = comptString.concat(comp)
+                                    })
+                                    const pair = `{"prompt": "${promptsString} ###", "completion": "${comptString} /nEND/n"}`
+                                    return (
+                                    <span>{ pair }<br/></span>
+                                )})
                             }
                         </div>
                     )
